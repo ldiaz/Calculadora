f9696a56483b6990e74f0c0311d24ab2d5e77c61
Leonardo Diaz <ingleonardo@gmail.com>
2016-05-24 16:26:19 -0500

    Agrega en UI, pantalla de calculadores + Botones de dígitos y operation π Botones enlazados con codigo y propiedad pantalla como Outlet en ViewController

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -2,24 +2,46 @@
 //  ViewController.swift
 //  Calculadora
 //
-//  Created by JOSE LEONARDO DIAZ on 24/5/16.
-//  Copyright © 2016 MayorgaFirm Inc. All rights reserved.
+//  Transcribed by JOSE LEONARDO DIAZ on 24/5/16.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    //propiedad pantalla donde se muestran los digitos presionados y el resultado de la operacion
+    //el signo de exclamacion extrae el valor asociado del Optional(UILabel)
+    @IBOutlet weak var pantalla: UILabel!
+    
+    // En programacion "Naming is everything", no se especifica el tipo por que el compilador puede determinar el tipo de dato automaticamente
+    var usuarioEstaEnLaMitadDeEscritura = false;
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func botonDigitoPresionado(sender: UIButton) {
+        //Se extrae el valor asociado del Optional(String) retornado por sender.currentTitle
+        let textoEnBoton = sender.currentTitle!
+        
+        //Valida si es la primera presión de boton para concatenar/remplazar el valor en pantalla
+        if usuarioEstaEnLaMitadDeEscritura {
+            let textoActualmenteEnPantalla = pantalla.text!
+            pantalla.text = textoActualmenteEnPantalla + textoEnBoton
+        } else {
+            
+            pantalla.text = textoEnBoton
+        }
+        
+        usuarioEstaEnLaMitadDeEscritura = true
     }
-
+    
+    @IBAction func operacionPresionada(sender: UIButton) {
+        //una vez presionada una operacion no se concatena el siguiente boton presionado a la pantalla
+        usuarioEstaEnLaMitadDeEscritura = false
+        
+        if let operacionMatematica = sender.currentTitle {
+            if operacionMatematica == "π" {
+                pantalla.text = String(M_PI)
+            }
+        }
+    }
 
 }
 
